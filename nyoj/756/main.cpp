144cba692164eedf85a41c56e9f669062f03bb0e
whjkm <452422845@qq.com>
2014-08-17 15:15:30 +0800

    重建二叉树

---
+++ nyoj/756/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstring>
+typedef struct bitnode
+{
+    int data;
+    struct bitnode *lchild,*rchild;
+}bitnode,*bittree;
+int main()
+{
+    char s1[28],s2[28];
+    while(scanf("%s%s",s1,s2)!=EOF)
+    {
+
+    }
+    return 0;
+}
