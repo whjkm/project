75cacde91f0ee69783796d08be22de4f4de46d50
whjkm <452422845@qq.com>
2014-08-17 15:11:52 +0800

    完全背包

--- nyoj/311/main.cpp
+++ nyoj/311/main.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <string.h>
 #define max(a,b) (a>b?a:b)
 int main()
@@ -18,6 +18,33 @@ int main()
         if(a[v]<0)
          printf("NO\n");
         else printf("%d\n",a[v]);
+    }
+    return 0;
+}
+*/
+
+#include <cstdio>
+#include <cstring>
+#define max(a,b) a>b?a:b
+const int maxn=50001;
+int dp[maxn];
+int main()
+{
+    int n,m,v,i,j,c,w;
+    scanf("%d",&n);
+    while(n--)
+    {
+      memset(dp,-10000,sizeof(dp));
+      dp[0]=0;
+      scanf("%d%d",&m,&v);
+      for(i=1;i<=m;i++)
+      {
+          scanf("%d%d",&c,&w);
+          for(j=c;j<=v;j++)
+                dp[j]=max(dp[j],dp[j-c]+w);
+      }
+      if(dp[v]<0) printf("NO\n");
+       else  printf("%d\n",dp[v]);
     }
     return 0;
 }
