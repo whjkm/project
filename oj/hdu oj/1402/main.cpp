29cdf1f572584f13c812935de2c4c78e9868b2f6
whjkm <452422845@qq.com>
2018-08-14 09:58:37 +0800

    update 1402
    update 1402

--- oj/hdu oj/1402/main.cpp
+++ oj/hdu oj/1402/main.cpp
@@ -18,6 +18,8 @@ struct complex
         r = _r;
         i = _i;
     }
+
+    //复数三种运算的定义
     complex operator +(const complex &b)
     {
         return complex(r + b.r, i + b.i);
@@ -35,13 +37,13 @@ struct complex
 // 雷德算法 -- 倒位序
 void Rader(complex F[], int len)
 {
-    int j = len >> 1;
+    int j = len >> 1;        // j = len/2
     for(int i=1; i<len-1; i++)
     {
-        if(i < j)
-            swap(F[i], F[j]);
+        if(i < j)                     // i<j保证只交换一次
+            swap(F[i], F[j]);         // 交换下标互为反转的元素
         int k = len >> 1;
-        while(j >= k)
+        while(j >= k)        // 由最高位检索,遇1变0,遇0变1
         {
             j -= k;
             k >>= 1;
@@ -51,6 +53,7 @@ void Rader(complex F[], int len)
     }
 }
 
+// on=1时为DFT,on=-1时为IDFT
 void FFT(complex F[], int len, int on)
 {
     Rader(F, len);
@@ -102,17 +105,17 @@ void Init(char str1[], char str2[])
     int len1 = strlen(str1);
     int len2 = strlen(str2);
     len = 1;
-    while(len < 2*len1 || len < 2*len2) len <<= 1;
+    while(len < 2*len1 || len < 2*len2) len <<= 1;     //将次数界变成2^n
 
     int i;
-    for(i=0; i<len1; i++)
+    for(i=0; i<len1; i++)   // 倒置存入
     {
         a[i].r = str1[len1-i-1] - '0';
         a[i].i = 0.0;
     }
     while(i < len)
     {
-        a[i].r = a[i].i = 0.0;
+        a[i].r = a[i].i = 0.0;      //多余的初始化为0
         i++;
     }
     for(i=0; i<len2; i++)
@@ -132,13 +135,13 @@ void Work()
     Conv(a, b, len);
     for(int i=0; i<len; i++)
     {
-        result[i] = a[i].r + 0.5;
+        result[i] = a[i].r + 0.5;    // 四舍五入
     }
 }
 
 void Export()
 {
-    for(int i=0; i<len; i++)
+    for(int i=0; i<len; i++)    // 进位
     {
         result[i+1] += result[i]/10;
         result[i] %= 10;
@@ -146,7 +149,7 @@ void Export()
     int high = 0;
     for(int i=len-1; i>=0; i--)
     {
-        if(result[i])
+        if(result[i])    //检索最高位
         {
             high = i;
             break;
@@ -154,11 +157,10 @@ void Export()
     }
     for(int i = high; i>=0; i--)
     {
-        printf("%d", result[i]);
+        printf("%d", result[i]);   //倒序输出
     }
     puts("");
 }
-
 
 
 int main()
